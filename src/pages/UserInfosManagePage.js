1b90eb8ea1065ef906a2039364d591e6c6953062
Lionel TOTON <totonlionel@gmail.com>
2024-09-17 22:05:39 +0200

    lione

--- src/pages/UserInfosManagePage.js
+++ src/pages/UserInfosManagePage.js
@@ -1,287 +1,5 @@
-// import React, { useEffect, useState } from "react";
-// import { getUserData, saveUserData } from "../services/userService";
-// import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-// import { storage } from "../config/firebase";
-// import GoBackBtn from "src/components/GoBackBtn";
-// import Loading from "src/components/Loading";
-
-// const UserInfosManagePage = () => {
-//   const [userData, setUserData] = useState(null);
-//   const [loading, setLoading] = useState(false);
-//   const [formData, setFormData] = useState({});
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const user = await getUserData("qkwBuoEernVFsBDq7bqFrTwa0ru1");
-//       if (user) {
-//         setUserData(user);
-//         setFormData(user); 
-//       }
-//     };
-//     fetchData();
-//   }, []);
-
-//   const handleInputChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData((prevData) => ({
-//       ...prevData,
-//       [name]: value,
-//     }));
-//   };
-
-//   const handleImageChange = async (e, fieldType) => {
-//     const file = e.target.files[0];
-//     if (file) {
-//       setLoading(true);
-//       const userId = "qkwBuoEernVFsBDq7bqFrTwa0ru1"; // Assurez-vous que l'ID utilisateur est correct
-//       const folder = fieldType === "logo" ? "logos" : "aboutImages"; // Choisir le bon dossier
-//       const imageRef = ref(storage, `${folder}/${userId}/${file.name}`);
-//       try {
-//         await uploadBytes(imageRef, file);
-//         const imageURL = await getDownloadURL(imageRef);
-//         setFormData((prevData) => ({
-//           ...prevData,
-//           [fieldType]: imageURL, // Utiliser fieldType pour mettre à jour le bon champ
-//         }));
-//       } catch (error) {
-//         console.error("Error uploading image: ", error);
-//       } finally {
-//         setLoading(false);
-//       }
-//     }
-//   };
-
-//   const handleSave = async () => {
-//     const userId = "qkwBuoEernVFsBDq7bqFrTwa0ru1"; // Assurez-vous que l'ID utilisateur est correct
-//     await saveUserData(userId, formData);
-//   };
-
-
-
-//   const renderInputFields = () => {
-//     return Object.keys(userData).map((key) => {
-//       const value = formData[key] || ""; // Valeur par défaut vide si pas encore définie
-//       const isImageField = key === "logo" || key === "aboutImage";
-  
-//       return (
-//         <div key={key} className="w-full flex flex-col justify-center items-center"> {/* Clé unique ici */}
-//           <div
-//             className="w-1/2 mb-4 border-2 border-yellow-400 rounded-xl flex flex-col items-center justify-center"
-//           >
-//             <label className="block font-ubuntu mb-1">
-//               {key.charAt(0).toUpperCase() +
-//                 key.slice(1).replace(/([A-Z])/g, " $1")}
-//             </label>
-//             {isImageField ? (
-//               <>
-//                 <div className="mb-2">
-//                   <input
-//                     id={`${key}-file-upload`} // Utilisation d'un id unique
-//                     type="file"
-//                     onChange={(e) => handleImageChange(e, key)} // Passe le type d'image ici
-//                     className="hidden"
-//                   />
-//                   <label
-//                     htmlFor={`${key}-file-upload`}
-//                     className="cursor-pointer flex items-center justify-center px-4 py-2 bg-green-500 text-white rounded-md shadow-sm hover:bg-green-700 transition duration-200"
-//                   >
-//                     Sélectionner une image
-//                   </label>
-//                 </div>
-//                 {loading ? (
-//                   <Loading />
-//                 ) : (
-//                   <img
-//                     className="w-32 h-32 object-cover rounded-md"
-//                     alt={key}
-//                     src={formData[key]}
-//                   />
-//                 )}
-//               </>
-//             ) : (
-//               <textarea
-//                 name={key}
-//                 value={value}
-//                 onChange={handleInputChange}
-//                 placeholder={key}
-//                 rows="5" // Nombre de lignes de base
-//                 className="w-auto p-2 border rounded-md placeholder-gray-400 focus:outline-none resize-none overflow-hidden"
-//                 onInput={(e) => {
-//                   e.target.style.height = "auto"; 
-//                   e.target.style.height = `${e.target.scrollHeight}px`; 
-//                 }}
-//               />
-//             )}
-//           </div>
-//         </div>
-//       );
-//     });
-//   };
-  
-
-//   return (
-//     <div>
-//       <div>
-//         {userData ? (
-//           <div className="mt-16 lg:mt-14">
-//             {renderInputFields()}
-//             <div className="fixed bottom-4 left-4 flex flex-row items-center justify-end ml-4">
-//               <button
-//                 onClick={handleSave}
-//                 className="mt-4 px-4 py-2 font-ubuntu text-xl bg-green-500 text-backgroung_secondary rounded hover:text-green-500 hover:bg-backgroung_secondary hover:border-2 hover:border-green-500"
-//               >
-//                 SAVE
-//               </button>
-//             </div>
-//             <GoBackBtn />
-//           </div>
-//         ) : (
-//           <div>Aucun profil trouvé pour cette ID</div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default UserInfosManagePage;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // const renderInputFields = () => {
-  //   return Object.keys(userData).map((key) => {
-  //     const value = formData[key] || ""; 
-  //     const isImageField = key === "logo" || key === "aboutImage";
-
-  //     return (
-  //       <div className="w-full flex flex-col justify-center items-center">
-  //         <div
-  //           key={key}
-  //           className="w-1/2 mb-4 border-2 border-yellow-400 rounded-xl flex flex-col items-center justify-center"
-  //         >
-  //           <label className="block font-ubuntu mb-1">
-  //             {key.charAt(0).toUpperCase() +
-  //               key.slice(1).replace(/([A-Z])/g, " $1")}
-  //           </label>
-  //           {isImageField ? (
-  //             <>
-  //               <div className="mb-2">
-  //                 <input
-  //                   id={`${key}-file-upload`} 
-  //                   type="file"
-  //                   onChange={(e) => handleImageChange(e, key)} 
-  //                   className="hidden"
-  //                 />
-  //                 <label
-  //                   htmlFor={`${key}-file-upload`}
-  //                   className="cursor-pointer flex items-center justify-center px-4 py-2 bg-green-500 text-white rounded-md shadow-sm hover:bg-green-700 transition duration-200"
-  //                 >
-  //                   Sélectionner une image
-  //                 </label>
-  //               </div>
-  //               {loading ? (
-  //                 <Loading />
-  //               ) : (
-  //                 <img
-  //                   className="w-32 h-32 object-cover rounded-md"
-  //                   alt={key}
-  //                   src={formData[key]}
-  //                 />
-  //               )}
-  //             </>
-  //           ) : (
-              
-  //             <textarea
-  //               name={key}
-  //               value={value}
-  //               onChange={handleInputChange}
-  //               placeholder={key}
-  //               rows="5" 
-  //               className="w-auto p-2 border rounded-md placeholder-gray-400 focus:outline-none resize-none overflow-hidden"
-  //               onInput={(e) => {
-  //                 e.target.style.height = "auto"; 
-  //                 e.target.style.height = `${e.target.scrollHeight}px`; 
-  //               }}
-  //             />
-  //           )}
-  //         </div>
-  //       </div>
-  //     );
-  //   });
-  // };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getUserData, saveUserData } from "../services/userService";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { storage } from "../config/firebase";
 import GoBackBtn from "src/components/GoBackBtn";
 import Loading from "src/components/Loading";
 
@@ -329,21 +47,27 @@ const UserInfosManagePage = () => {
     setLoading(true);
     const userId = "qkwBuoEernVFsBDq7bqFrTwa0ru1"; 
     const updatedFormData = { ...formData };
+    // for (const [key, file] of Object.entries(imageFiles)) {
+    //   console.log(imageFiles)
+    //   console.log(imageFiles)
 
-    for (const [key, file] of Object.entries(imageFiles)) {
-      const folder = key === "logo" ? "logos" : "aboutImages";
-      const imageRef = ref(storage, `${folder}/${userId}/${file.name}`);
-      try {
-        await uploadBytes(imageRef, file);
-        const imageURL = await getDownloadURL(imageRef);
-        updatedFormData[key] = imageURL;
-      } catch (error) {
-        console.error("Error uploading image: ", error);
-      }
-    }
+    //   const folder = key === "logo" ? "logos" : "aboutImages";
+    //   const imageRef = ref(storage, `${folder}/${userId}/${file.name}`);
+    //   try {
+    //     await uploadBytes(imageRef, file);
+    //     const imageURL = await getDownloadURL(imageRef);
+    //     updatedFormData[key] = imageURL;
+    //   } catch (error) {
+    //     console.error("Error uploading image: ", error);
+    //   }
+    // }
 
     try {
-      await saveUserData(userId, updatedFormData);
+      await saveUserData(
+        userId,
+        updatedFormData, 
+        imageFiles.logo, 
+        imageFiles.aboutImage);
       alert("Données enregistrées avec succès !");
     } catch (error) {
       console.error("Error saving user data: ", error);
@@ -353,6 +77,8 @@ const UserInfosManagePage = () => {
     }
   };
 
+
+
   const renderInputFields = () => {
     return Object.keys(userData).map((key) => {
       const value = formData[key] || ""; // Valeur par défaut vide si pas encore définie
@@ -360,7 +86,7 @@ const UserInfosManagePage = () => {
   
       return (
         <div key={key} className="w-full flex flex-col justify-center items-center">
-          <div className="w-1/2 mb-4 border-2 border-yellow-400 rounded-xl flex flex-col items-center justify-center">
+          <div className="w-1/2 mb-4 border-2 border-blue-400 rounded-xl flex flex-col items-center justify-center">
             <label className="block font-ubuntu mb-1">
               {key.charAt(0).toUpperCase() + key.slice(1).replace(/([A-Z])/g, " $1")}
             </label>
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,42 +1,66 @@
-
 /* eslint-disable import/no-anonymous-default-export */
 import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
-import { ref, listAll, deleteObject,uploadBytes, getDownloadURL } from "firebase/storage";
+import { updateEmail, updatePassword, signOut } from "firebase/auth";
 
-import { db, storage } from '../config/firebase';
+import {
+  ref,
+  listAll,
+  deleteObject,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
+
+import { db, storage } from "../config/firebase";
 import { auth } from "../config/firebase";
 
-// Créer ou mettre à jour les données utilisateur
-// export const saveUserData = async (userId, userData, logoFile, aboutImageFile) => {
-//   try {
-    
-//     await setDoc(doc(db, "users", userId), {
-//       ...userData,
-//       logo: await formatUrlLinkToImage(logoFile, userId, 'logos') || userData.logo,
-//       aboutImage: await formatUrlLinkToImage(aboutImageFile, userId, 'aboutImages') || userData.aboutImage
-//     }, { merge: true }); // Merge pour ne pas écraser les anciennes données
-//   } catch (error) {
-//     console.error("Error saving user data: ", error);
-//   }
-// };
-
-export const saveUserData = async (userId, userData, logoFile, aboutImageFile) => {
+export const saveUserData = async (
+  userId,
+  userData,
+  logoFile,
+  aboutImageFile
+) => {
   try {
-    // Ajout de logs pour vérifier quel fichier est uploadé
     console.log("Logo file:", logoFile ? logoFile.name : "No file");
-    console.log("About image file:", aboutImageFile ? aboutImageFile.name : "No file");
+    console.log(
+      "About image file:",
+      aboutImageFile ? aboutImageFile.name : "No file"
+    );
 
-    await setDoc(doc(db, "users", userId), {
+    const updatedUserData = {
       ...userData,
-      logo: await formatUrlLinkToImage(logoFile, userId, 'logos') || userData.logo,
-      aboutImage: await formatUrlLinkToImage(aboutImageFile, userId, 'aboutImages') || userData.aboutImage
-    }, { merge: true }); // Merge pour ne pas écraser les anciennes données
+      logo:
+        (await formatUrlLinkToImage(logoFile, userId, "logos")) ||
+        userData.logo,
+      aboutImage:
+        (await formatUrlLinkToImage(aboutImageFile, userId, "aboutImages")) ||
+        userData.aboutImage,
+    };
+
+    await setDoc(doc(db, "users", userId), updatedUserData, { merge: true });
+
+    await deleteUnusedFiles("logos", userId, logoFile?.name);
+    await deleteUnusedFiles("aboutImages", userId, aboutImageFile?.name);
   } catch (error) {
     console.error("Error saving user data: ", error);
   }
 };
 
+const deleteUnusedFiles = async (folder, userId, fileNameToKeep) => {
+  try {
+    const directoryRef = ref(storage, `${folder}/${userId}/`);
 
+    const fileList = await listAll(directoryRef);
+
+    fileList.items.forEach(async (fileRef) => {
+      if (fileRef.name !== fileNameToKeep) {
+        await deleteObject(fileRef);
+        console.log(`Deleted: ${fileRef.name}`);
+      }
+    });
+  } catch (error) {
+    console.error("Error deleting files: ", error);
+  }
+};
 
 const formatUrlLinkToImage = async (imageFile, userId, folder) => {
   let imageUrl = "";
@@ -46,254 +70,53 @@ const formatUrlLinkToImage = async (imageFile, userId, folder) => {
     imageUrl = await getDownloadURL(logoRef);
   }
 
-  return imageUrl
-}
-
-
-// // const formatUrlLinkToImage = async (imageFile, userId, folder) => {
-// //   let imageUrl = "";
-// //   try {
-// //     if (imageFile) {
-// //       const fileRef = ref(storage, `${folder}/${userId}/${imageFile.name}`);
-// //       await uploadBytes(fileRef, imageFile);
-// //       imageUrl = await getDownloadURL(fileRef);
-// //     } else {
-// //       console.error("No image file provided");
-// //     }
-// //   } catch (error) {
-// //     console.error("Error uploading file:", error);
-// //   }
-
-// //   return imageUrl;
-// // };
-
-
-// const formatUrlLinkToImage = async (imageFile, userId, folder) => {
-//   let imageUrl = "";
-//   if (imageFile) {
-//     try {
-//       // Ajout de logs pour vérifier le chemin et le fichier
-//       console.log(`Uploading file to ${folder}/${userId}/${imageFile.name}`);
-
-//       const fileRef = ref(storage, `${folder}/${userId}/${imageFile.name}`);
-//       await uploadBytes(fileRef, imageFile);
-//       imageUrl = await getDownloadURL(fileRef);
-
-//       console.log(`File uploaded successfully: ${imageUrl}`);
-//     } catch (error) {
-//       console.error("Error uploading file:", error);
-//     }
-//   } else {
-//     console.error("No image file provided");
-//   }
-
-//   return imageUrl;
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export const saveUserData = async (userId, userData, logoFile, aboutImageFile) => {
-//   try {
-//     // Obtenez les données utilisateur existantes pour vérifier les anciennes images
-//     const userDocRef = doc(db, "users", userId);
-//     const userDoc = await getDoc(userDocRef);
-//     const existingUserData = userDoc.data();
+  return imageUrl;
+};
+
+
+
+export const updateUserAuthentication = async (user, newEmail, newPassword) => {
+  try {
+    let emailUpdated = false;
+    let passwordUpdated = false;
+
+    // Met à jour l'email si une nouvelle valeur est fournie
+    if (newEmail && newEmail !== user.email) {
+      await updateEmail(user, newEmail);
+      console.log("Email updated to:", newEmail);
+      emailUpdated = true;
+    }
+
+    // Met à jour le mot de passe si une nouvelle valeur est fournie
+    if (newPassword) {
+      await updatePassword(user, newPassword);
+      console.log("Password updated.");
+      passwordUpdated = true;
+    }
+
+    // Si l'email ou le mot de passe est mis à jour, déconnecte l'utilisateur
+    if (emailUpdated || passwordUpdated) {
+      await signOut(user.auth);
+      console.log("User signed out after updating credentials.");
+    }
     
-//     // Supprimer les anciennes images si elles existent
-//     if (existingUserData) {
-//       if (existingUserData.logo) {
-//         const oldLogoRef = ref(storage, existingUserData.logo);
-//         await deleteObject(oldLogoRef);
-//       }
-//       if (existingUserData.aboutImage) {
-//         const oldAboutImageRef = ref(storage, existingUserData.aboutImage);
-//         await deleteObject(oldAboutImageRef);
-//       }
-//     }
-
-//     // Télécharger les nouvelles images et obtenir les URLs
-//     const newLogoUrl = await formatUrlLinkToImage(logoFile, userId, 'logos');
-//     const newAboutImageUrl = await formatUrlLinkToImage(aboutImageFile, userId, 'aboutImages');
-
-//     // Mettre à jour les données utilisateur dans Firestore
-//     await setDoc(userDocRef, {
-//       ...userData,
-//       logo: newLogoUrl || userData.logo,
-//       aboutImage: newAboutImageUrl || userData.aboutImage
-//     }, { merge: true });
-
-//   } catch (error) {
-//     console.error("Error saving user data: ", error);
-//   }
-// };
-
-// const formatUrlLinkToImage = async (imageFile, userId, folder) => {
-//   let imageUrl = "";
-//   if (imageFile) {
-//     const imageRef = ref(storage, `${folder}/${userId}/${imageFile.name}`);
-//     await uploadBytes(imageRef, imageFile);
-//     imageUrl = await getDownloadURL(imageRef);
-//   }
-
-//   return imageUrl;
-// }
+  } catch (error) {
+    console.error("Error updating user authentication:", error);
+  }
+};
 
 
 
-// -----------------------------------------------------------DELETED-----------------------------------------------------
+// Exemple d'appel de la méthode
+const updateUserData = async (newEmail, newPassword) => {
+  const user = auth.currentUser; // Récupère l'utilisateur actuellement connecté
 
-// export const saveUserData = async (userId, userData, logoFile, aboutImageFile) => {
-//   try {
-//     // Supprimer tout ce qu'il y a dans le répertoire "logos" de l'utilisateur
-//     const logoFolderRef = ref(storage, `logos/${userId}`);
-//     await deleteAllFilesInFolder(logoFolderRef);
-    
-//     // Supprimer tout ce qu'il y a dans le répertoire "aboutImages" de l'utilisateur
-//     const aboutImageFolderRef = ref(storage, `aboutImages/${userId}`);
-//     await deleteAllFilesInFolder(aboutImageFolderRef);
-
-//     // Télécharger les nouvelles images et obtenir les URLs
-//     const newLogoUrl = await formatUrlLinkToImage(logoFile, userId, 'logos');
-//     const newAboutImageUrl = await formatUrlLinkToImage(aboutImageFile, userId, 'aboutImages');
-
-//     // Mettre à jour les données utilisateur dans Firestore
-//     await setDoc(doc(db, "users", userId), {
-//       ...userData,
-//       logo: newLogoUrl || userData.logo,
-//       aboutImage: newAboutImageUrl || userData.aboutImage
-//     }, { merge: true });
-
-//   } catch (error) {
-//     console.error("Error saving user data: ", error);
-//   }
-// };
-
-// // Fonction pour supprimer tous les fichiers dans un répertoire donné
-// const deleteAllFilesInFolder = async (folderRef) => {
-//   try {
-//     const listResult = await listAll(folderRef);
-//     const deletePromises = listResult.items.map((itemRef) => deleteObject(itemRef));
-//     await Promise.all(deletePromises);
-//   } catch (error) {
-//     console.error("Error deleting files in folder: ", error);
-//   }
-// };
-
-// const formatUrlLinkToImage = async (imageFile, userId, folder) => {
-//   let imageUrl = "";
-//   if (imageFile) {
-//     const imageRef = ref(storage, `${folder}/${userId}/${imageFile.name}`);
-//     await uploadBytes(imageRef, imageFile);
-//     imageUrl = await getDownloadURL(imageRef);
-//   }
-
-//   return imageUrl;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  if (user) {
+    await updateUserAuthentication(user, newEmail, newPassword);
+  } else {
+    console.log("No user is logged in.");
+  }
+};
 
 
 
@@ -312,17 +135,23 @@ const formatUrlLinkToImage = async (imageFile, userId, folder) => {
 
 export const getUserData = async (userId) => {
   try {
-    const userDetailFromAuth = getUserDetailFromAuthentification(userId); // Cette fonction peut être synchrone
-    const userDetailFromFirestore = await getUserDetailFromFirestore(userId); // Assurez-vous que c'est bien une promesse
+    const userDetailFromAuth = getUserDetailFromAuthentification(userId); 
+    const userDetailFromFirestore = await getUserDetailFromFirestore(userId);
 
-    // Si les emails correspondent, retourner les détails Firestore, sinon null
-    if (userDetailFromAuth && userDetailFromFirestore && userDetailFromAuth.email === userDetailFromFirestore.email) {
+    if (
+      userDetailFromAuth &&
+      userDetailFromFirestore &&
+      userDetailFromAuth.email === userDetailFromFirestore.email
+    ) {
       return userDetailFromFirestore;
     } else {
       return null; // Si les emails ne correspondent pas ou en cas d'erreur
     }
   } catch (error) {
-    console.error("Erreur lors de la récupération des données utilisateur :", error);
+    console.error(
+      "Erreur lors de la récupération des données utilisateur :",
+      error
+    );
     return null;
   }
 };
@@ -350,31 +179,19 @@ const getUserDetailFromFirestore = async (userId) => {
     const userDoc = await getDoc(userDocRef);
 
     if (userDoc.exists()) {
-      return userDoc.data(); 
+      return userDoc.data();
     } else {
       console.error("Aucun utilisateur trouvé dans Firestore pour cet ID.");
       return null;
     }
   } catch (error) {
-    console.error("Erreur lors de la récupération des détails depuis Firestore :", error);
+    console.error(
+      "Erreur lors de la récupération des détails depuis Firestore :",
+      error
+    );
     return null;
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const defaultDataProvider = async (userId) => {
   const docRef = doc(db, "users", userId);
@@ -386,25 +203,20 @@ const defaultDataProvider = async (userId) => {
     linkBehance: "",
     linkLinkedIn: "",
     linkInstagram: "",
-    email: "tony@example.com",
+    email: "tony@gmail.com",
     aboutImage: "",
-    password: ""
+    password: "",
   };
   await setDoc(docRef, defaultData);
   return defaultData;
-}
-
-
-
-
-
-
-// Mettre à jour les données utilisateur
-export const updateUserData = async (userId, userData) => {
-  try {
-    const docRef = doc(db, "users", userId);
-    await updateDoc(docRef, userData);
-  } catch (error) {
-    console.error("Error updating user data: ", error);
-  }
 };
+
+// // Mettre à jour les données utilisateur
+// export const updateUserData = async (userId, userData) => {
+//   try {
+//     const docRef = doc(db, "users", userId);
+//     await updateDoc(docRef, userData);
+//   } catch (error) {
+//     console.error("Error updating user data: ", error);
+//   }
+// };
